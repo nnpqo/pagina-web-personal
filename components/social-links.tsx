0bec34b731e5d0158978c898d6be31d2e1907549
Yanina <201801963@est.umss.edu>
2025-05-13 01:37:25 -0400

    arreglo del icono de tiktok

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -43,7 +43,7 @@ export function SocialLinks() {
         className="rounded-full border-cyber-200 dark:border-cyber-800 hover:bg-cyber-100 dark:hover:bg-cyber-900/30"
       >
         <Link href="https://www.tiktok.com/@tech.by.nina?_t=ZM-8wId8bHPtSv&_r=1" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
-        <TiktokIcon className="h-7 w-7 text-pink-500" />
+        <TiktokIcon className="h-5 w-5 text-black-500" />
         </Link>
       </Button>
     </div>
