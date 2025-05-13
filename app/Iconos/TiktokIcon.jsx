beef71a0cd14ed93275a1f9ae2fa33ca7f66c12a
Yanina <201801963@est.umss.edu>
2025-05-13 01:32:07 -0400

    cambio de color del icono

--- app/Iconos/TiktokIcon.jsx
+++ app/Iconos/TiktokIcon.jsx
@@ -2,7 +2,7 @@
 const TiktokIcon = (props) => (
     <svg
       viewBox="0 0 24 24"
-      fill="currentColor"
+      stroke="currentColor"
       className={props.className}
       xmlns="http://www.w3.org/2000/svg"
     >
