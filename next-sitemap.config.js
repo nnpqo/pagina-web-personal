3a8265f9ee75ed592b78895214e1e2b74bbd3409
Yanina <201801963@est.umss.edu>
2025-05-14 15:30:56 -0400

    administracion SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://techbynina.tech', 
+    generateRobotsTxt: true, 
+    sitemapSize: 5000,
+    changefreq: 'weekly',
+    priority: 0.7,
+  };
+  
